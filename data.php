0a72f1f1a7642738999ce3399c9e198e87e8e08e
cusstom180 <cusstom180@icloud.com>
2017-08-17 21:39:49 -0400

    another basic commit

---
+++ data.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+<?php 
+	$username = $_GET['username'];
+	
+	echo $username;
+?>
+<body>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Crossfit DV8 | Login</title>
+<link href="/css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<form action="/data.php" method="post">
+		<label>user name</label>
+		<input type="text" name="username" >
+		<label>password</label>
+		<input type="password" name="username" >
+		<input type="submit" formmethod="POST" >
+	</form>
+</body>
+</html>
