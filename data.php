7e660f670aed4c86b9ea03693efd5d3e2abca79c
cusstom180 <cusstom180@icloud.com>
2017-08-25 14:36:09 -0400

    Add files via upload
    added login password error

--- data.php
+++ data.php
@@ -1,5 +1,41 @@
 <?php 
-	$username = $_POST['username'];
+
+include('connect.php');
+
+#capture variables
+$user =  $_POST['username']; // mysqli_real_escape_string($con, );
+$password = md5($_POST['password']); //md5(mysqli_real_escape_string($con, ));
+
+
+
+
+#check if username already exists
+$usercheckquery = "SELECT * FROM `user_account` WHERE `user_name` = '$user' and `password` = '$password' ";
+
+$result = mysqli_query($con, $usercheckquery);
+
+if (mysqli_num_rows($result) == 1) {
+	echo 'there is a result';
+} else {
+	$incorretPwUrl = 'index.php/?login=1';
+	print $incorretPwUrl;
+	header('Location: '.$incorretPwUrl);
+}
+/*
+$useridcheck = mysqli_query($con, $useridcheckquery);
+
+$userquerycheck = mysqli_fetch_assoc($useridcheck);
+
+if ($userquerycheck['email'] == $email) {
+	
+	$success = 'you have logged in';
+	
+}	else {
+	
+	header('Location: register.php');
+	
+	
+} */
 ?>
 
 
@@ -12,6 +48,6 @@
 
 <body>
 
-<?php echo $username; ?>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,19 +1,37 @@
+<?php
+
+$login = isset($_GET['login']);
+ if($login){
+$login = $_GET['login'];
+}  
+/* $login = $_GET['login'];
+print $login; */
+?>
+
+
 <!doctype html>
 <html>
 <head>
 <meta charset="UTF-8">
-<link href="/css/style.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css">
 <title>Crossfit DV8 | Login</title>
 
 </head>
 
 <body>
 	<form action="data.php" method="post">
+<?php 
+if ($login == 1) {
+	echo "Incorrect pw";
+}
+?>
+
+
 		<label>user name</label>
 		<input type="text" name="username" >
 		<label>password</label>
 		<input type="password" name="password" >
-		<input type="submit" >
+		<input type="submit" value="Submit" >
 	</form>
 </body>
 </html>
